63fb42cf318347b1ef5a7b9b3e5758441b1ce998
Noah Vendrig <noahvendrig@gmail.com>
2022-06-09 13:58:47 +1000

    search is working now

--- test.py
+++ test.py
@@ -10,20 +10,15 @@ column = dataset["ingredients"]
 #split spring for ingredients by space and comma
 
 # split = ingredients.split(', ') - uncomment this later and work it out into algorithm
+
 ingredient_wanted = 'sugar'
 wanted = dataset.loc[dataset['ingredients'] == ingredient_wanted]
 
-for i,j in dataset[4:8].iterrows():
+for i,j in dataset[4:100].iterrows():
     a =j["ingredients"]
     a = a.split(", ")
-
-    for n in a:
-        # if "'" in n:
-        # n.replace("'", "")
-        # print(a)
-        print(n.split()[0])
-
-    # for ingredient in a:
-    #     print(ingredient)
-    #     if ingredient == ingredient_wanted:
-    #         print(i)
+    
+    for ingredient in a:
+        if ingredient == ingredient_wanted:
+            # print(i) # index of the row that has recipe with available ingredient
+            print(dataset['name'][i])
